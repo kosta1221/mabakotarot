6041636be39a03c1109b8eb9583b76d1e88c4225
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-05-31 12:58:35 +0300

    running puppeteer with adblock, performing task asynchronously
    Now the task gets done for each url asynchronously

--- headline-scraper.js
+++ headline-scraper.js
@@ -1,19 +1,35 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+
+const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
+puppeteer.use(AdblockerPlugin());
 
 const scrapeHeadline = async () => {
-	browser = await puppeteer.launch({
+	const browser = await puppeteer.launch({
 		headless: false,
 		args: ["--disable-web-security"],
 		slowMo: 50,
 	});
 
-	page = await browser.newPage();
-
-	await page.goto("https://www.n12.co.il/", { waitUntil: "networkidle0" });
-	await page.screenshot({ path: "./headlines/screenshot.png", fullPage: true });
+	await Promise.all(scrapePromises(browser, "https://www.n12.co.il/", "https://www.ynet.co.il"));
 
 	console.log("done, check screenshot");
 	await browser.close();
+};
+
+const scrapePromises = (browser, ...urls) => {
+	return urls.map(
+		(url, i) =>
+			new Promise(async (resolve, reject) => {
+				try {
+					const page = await browser.newPage();
+					await page.goto(url, { waitUntil: "networkidle0" });
+					await page.screenshot({ path: `./headlines/screenshot${i}.png` });
+					resolve();
+				} catch (error) {
+					reject(error);
+				}
+			})
+	);
 };
 
 scrapeHeadline();
