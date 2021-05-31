bd3f626110c8e2f76aa98a8c0fc6a2680511fd7c
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-05-31 13:23:10 +0300

    fullscreen screenshots, improve modularity

--- headline-scraper.js
+++ headline-scraper.js
@@ -3,27 +3,38 @@ const puppeteer = require("puppeteer-extra");
 const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 puppeteer.use(AdblockerPlugin());
 
+const n12 = {
+	url: "https://www.n12.co.il/",
+	folder: "n12",
+};
+
+const ynet = {
+	url: "https://www.ynet.co.il",
+	folder: "ynet",
+};
+
 const scrapeHeadline = async () => {
 	const browser = await puppeteer.launch({
 		headless: false,
-		args: ["--disable-web-security"],
+		defaultViewport: null,
+		args: ["--disable-web-security", "--start-maximized"],
 		slowMo: 50,
 	});
 
-	await Promise.all(scrapePromises(browser, "https://www.n12.co.il/", "https://www.ynet.co.il"));
+	await Promise.all(scrapePromises(browser, n12, ynet));
 
 	console.log("done, check screenshot");
 	await browser.close();
 };
 
-const scrapePromises = (browser, ...urls) => {
-	return urls.map(
-		(url, i) =>
+const scrapePromises = (browser, ...sites) => {
+	return sites.map(
+		(site, i) =>
 			new Promise(async (resolve, reject) => {
 				try {
 					const page = await browser.newPage();
-					await page.goto(url, { waitUntil: "networkidle0" });
-					await page.screenshot({ path: `./headlines/screenshot${i}.png` });
+					await page.goto(site.url, { waitUntil: "networkidle0" });
+					await page.screenshot({ path: `./headlines/${site.folder}/screenshot.png` });
 					resolve();
 				} catch (error) {
 					reject(error);
