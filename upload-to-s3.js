9282475c49da8da7c225d6a0f56fb6da25633542
michefraim <77235028+michefraim@users.noreply.github.com>
2021-06-28 17:41:30 +0300

    bandaid fix for empty files upload

--- upload-to-s3.js
+++ upload-to-s3.js
@@ -7,43 +7,33 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
 });
 
-const watchFile = (filePath) => {
-	const watchStartTime = Date.now();
-	while (!fs.existsSync(filePath)) {
-	  if (Date.now() - watchStartTime > 15) {
-		return false;
-	  }
-	}
-	return true;
-  };
 const uploadFile = async (filePath, s3Path) => {
   // Read content from the file
   try {
-	if (!watchFile(filePath)) {
-		return;
-	}
-    const fileContent = fs.readFileSync(filePath);
-    const params = {
-      Bucket: process.env.BUCKET_NAME,
-      Key: s3Path, // File name you want to save as in S3
-      Body: fileContent,
-    };
-    // Setting up S3 upload parameters
-
-    return new Promise(async (resolve, reject) => {
-      try {
-        // Uploading files to the bucket
-        s3.upload(params, function (err, data) {
-          if (err) {
-            throw err;
-          }
-          console.log(`File uploaded successfully. ${data.Location}`);
-          resolve(data.Location);
-        });
-      } catch (error) {
-        reject(error);
-      }
-    });
+    setTimeout(() => {
+      const fileContent = fs.readFileSync(filePath);
+      const params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: s3Path, // File name you want to save as in S3
+        Body: fileContent,
+      };
+      // Setting up S3 upload parameters
+  
+      return new Promise(async (resolve, reject) => {
+        try {
+          // Uploading files to the bucket
+          s3.upload(params, function (err, data) {
+            if (err) {
+              throw err;
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+            resolve(data.Location);
+          });
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }, 1500);
   } catch (err) {
     console.log(err);
   }
