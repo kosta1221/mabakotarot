ba5bf545cd78074b5a1d91caca2ac34411af206e
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-07-02 21:10:27 +0300

    Update image-diff.js

--- image-diff.js
+++ image-diff.js
@@ -3,10 +3,13 @@ const PNG = require("pngjs").PNG;
 const pixelmatch = require("pixelmatch");
 const sharp = require("sharp");
 const axios = require("axios");
-const { getFileNameFromOptions, getS3UrlFromSiteAndFileName } = require("./utils");
+
 const sizeOf = require("buffer-image-size");
 
 const getDiffFromUrl = async (s3Url1, s3Url2) => {
+	console.log("url 1: ", s3Url1);
+	console.log("url 2: ", s3Url2);
+
 	const { data: data1 } = await axios({
 		url: s3Url1,
 		method: "GET",
@@ -26,8 +29,13 @@ const getDiffFromUrl = async (s3Url1, s3Url2) => {
 	// const img2 = PNG.sync.read(fs.readFileSync("headlines/n12/2021-07-01_21-15.png"));
 	const { width: width1, height: height1 } = sizeOf(data1);
 	const { width: width2, height: height2 } = sizeOf(data2);
+
 	console.log("width1: ", width1, "height1: ", height1);
 	console.log("width2: ", width2, "height2: ", height2);
+
+	if (width1 !== width2 || height1 !== height2) {
+		throw new Error("Different image dimensions!");
+	}
 
 	const dataSharp1 = await sharp(data1).png().toBuffer();
 	const dataSharp2 = await sharp(data2).png().toBuffer();
@@ -37,16 +45,14 @@ const getDiffFromUrl = async (s3Url1, s3Url2) => {
 	const diff = new PNG({ width: width1, height: height1 });
 
 	const diffNum = pixelmatch(img1.data, img2.data, diff.data, width1, height1, { threshold: 0.1 });
-	console.log(diffNum);
-	console.log((diffNum / (width1 * height1)) * 100);
+	const diffPercentage = (diffNum / (width1 * height1)) * 100;
 
-	fs.writeFileSync("headlines/diff/diff.png", PNG.sync.write(diff));
+	console.log("diffNum: ", diffNum);
+	console.log("diffPercentage: ", diffPercentage);
+
+	fs.writeFileSync(`ignore/diff.png`, PNG.sync.write(diff));
+
+	return { diffNum, diffPercentage, diff };
 };
 
-const fileName1 = getFileNameFromOptions({ day: 1, hour: 23, minute: 45 });
-const fileName2 = getFileNameFromOptions({ day: 1, hour: 23, minute: 30 });
-
-const s3Url1 = getS3UrlFromSiteAndFileName("n12", fileName1);
-const s3Url2 = getS3UrlFromSiteAndFileName("ynet", fileName2);
-
-getDiffFromUrl(s3Url1, s3Url2);
+module.exports = { getDiffFromUrl };
