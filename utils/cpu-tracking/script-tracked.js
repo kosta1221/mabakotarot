5d9570ac3cf62f645e7ad6d2b7d3237a124ff121
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-07-06 19:42:38 +0300

    can use cpu tracking util to run script and show cpu usage

---
+++ utils/cpu-tracking/script-tracked.js
@@ -0,0 +1,7 @@
+const cpuTrackerInterval = require("./cpu-tracker");
+
+require("../../index");
+// require("../../index-split-sites");
+// require("../../index-split-sites-diff-browsers");
+
+// clearInterval(cpuTrackerInterval);
