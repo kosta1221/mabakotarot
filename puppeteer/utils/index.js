457bfb2a5d98c205d376f4cb7eee9f7022c8c8ea
michefraim <77235028+michefraim@users.noreply.github.com>
2021-07-11 14:04:37 +0300

    walla and news13 font fix attempt #2

--- puppeteer/utils/index.js
+++ puppeteer/utils/index.js
@@ -3,10 +3,18 @@ const chromium = require("chrome-aws-lambda");
 const { addExtra } = require("puppeteer-extra");
 const puppeteerExtra = addExtra(chromium.puppeteer);
 const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
+const fs = require("fs");
 puppeteerExtra.use(AdblockerPlugin());
 
 const launchBrowser = async () => {
-	await chromium.font("https://fontlibrary.org//face/liberation-sans");
+	// LiberationSans for walla
+	await chromium.font("https://rawcdn.githack.com/shantigilbert/liberation-fonts-ttf/ef7161f03e305982b0b247e9a0b7cc472376dd83/LiberationSans-Regular.ttf");
+	await chromium.font("https://rawcdn.githack.com/shantigilbert/liberation-fonts-ttf/ef7161f03e305982b0b247e9a0b7cc472376dd83/LiberationSans-Bold.ttf");
+	await chromium.font("https://rawcdn.githack.com/shantigilbert/liberation-fonts-ttf/ef7161f03e305982b0b247e9a0b7cc472376dd83/LiberationSans-Italic.ttf");
+		// Almoni for News13 (lesser odds of working)
+	await chromium.font("https://13news.co.il/wp-content/themes/reshet_tv/build/assets/fonts/Almoni/almoni-dl-aaa-300.woff");
+	await chromium.font("https://13news.co.il/wp-content/themes/reshet_tv/build/assets/fonts/Almoni/almoni-dl-aaa-400.woff");
+	await chromium.font("https://13news.co.il/wp-content/themes/reshet_tv/build/assets/fonts/Almoni/almoni-dl-aaa-700.woff");
 
 	const browser = await puppeteerExtra.launch({
 		args: chromium.args,
